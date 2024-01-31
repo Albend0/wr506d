facee050054e81b32d0750dfcf0220bade921414
Albend0 <albandewaubert@gmail.com>
2024-01-31 18:09:16 +0100

    feat:app-fixtures-update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -43,6 +43,20 @@ class AppFixtures extends Fixture
 
             $movie = new Movie();
             $movie->setTitle($item);
+            $movies->setReleaseDate($faker->dateTimeThisCentury());
+            $movies->setDescription($faker->text($maxNbChars = 200));
+            $movies->setDuration($faker->numberBetween($min = 60, $max = 180));
+            $movies->setNote(
+                $faker->randomFloat(
+                    $nbMaxDecimals = 1,
+                    $min = 0,
+                    $max = 10
+                )
+            );
+            $movies->setEntries($faker->numberBetween($min = 10000, $max = 10000000));
+            $movies->setBudget($faker->numberBetween($min = 100000, $max = 1000000));
+            $movies->setDirector($faker->director());
+            $movies->setWebsite($faker->imageUrl(360, 360, 'animals', true, 'dogs', true, 'jpg'));
 
             shuffle($createdActors);
             $createdActorsSliced = array_slice($createdActors, 0, 5);
